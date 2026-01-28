a76f8fc6d7e9530899247889ac7458aff59e1563
Arruzzolo Davide <arruzzolod.lavoro@gmail.com>
2026-01-28 22:06:34 +0100

    Push

---
+++ src/main/java/dzve/model/GroupData.java
@@ -0,0 +1,17 @@
+package dzve.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupData {
+    @JsonProperty("groups")
+    private Map<UUID, Group> groups;
+}
